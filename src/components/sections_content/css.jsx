6180efa31bb11f100edede174a2046b5d7292677
renaud dor <renaud.dor@gmail.com>
2020-09-09 23:14:01 +0200

    Continue CSS content

--- src/components/sections_content/css.jsx
+++ src/components/sections_content/css.jsx
@@ -68,7 +68,81 @@ background: linear-gradient(135deg,       /* angle */
 
   typography: `
 h1 {
-font-family: "Garamond", "Times", serif;
+  font-family: "Garamond", "Times", serif;
+}`,
+
+  fontFace: `
+@font-face {
+  font-family: "Roboto";
+  src: url(fonts/Roboto.woff2) format('woff2'),
+       url(fonts/Roboto.woff) format('woff'),
+       url(fonts/Roboto.tff) format('truetype');
+}`,
+
+  gridColumns: `
+.grid {
+  display: grid;
+  width: 500px;
+  grid-template-columns: 200px 300px;
+}`,
+
+  gridTemplate: `
+.grid {
+  display: grid;
+  width: 1000px;
+  height: 500px;
+  grid-template: 200px 300px / 20% 10% 70%;
+}`,
+
+  gridItem: `
+.item {
+  grid-row-start: 1;
+  grid-row-end: 3;
+  grid-column-start: 1;
+  grid-column-end: 3;
+}`,
+
+  gridExampleHtml: `
+<div class="container">
+  <header>Wielcom!</header>
+  <nav>Links</nav>
+  <section class="info">Info!</section>
+  <section class="services">Services!</section>
+  <footer>Contact us!</footer>
+</div>`,
+
+  gridExampleCss: `
+.container {
+  display: grid;
+  max-width: 900px;
+  position: relative;
+  margin: auto;
+  grid-template-areas: "head head"
+                       "nav nav"
+                       "info services"
+                       "footer footer";
+  grid-template-rows: 300px 120px 800px 120px;
+  grid-template-columns: 1fr 3fr;
+}
+
+header {
+  grid-area: head;
+}
+
+nav {
+  grid-area: nav;
+}
+
+.info {
+  grid-area: info;
+}
+
+.services {
+  grid-areas: services;
+}
+
+footer {
+  grid-area: footer;
 }`
 };
 
@@ -147,6 +221,7 @@ const Css = ({ subSectionName }) => {
             <li><Ic code="inline" />: only take the horizontal space of its width (default for tags like <Ic code="<em> <strong> <a>" /> etc…). Inline elements cannot be altered in size by height and width css proprieties.</li>
             <li><Ic code="block" />: will fill the entire horizontal space of the page. Their height by default is the height of their content. But their height and width can by modified by CSS proprieties. (default for tags like <Ic code="<h1> <h6> <p> <div> <footer>" /> …)</li>
             <li><Ic code="inline-block" />: combine properties of both: they will appear next to each other but you can specify their dimensions using width and height (by default for images)</li>
+            <li><Ic code="none" /> : the element will be completely removed from the page. (Different with <Ic code="visibility: hidden" />, that will make the element invisible, but the space reserved for it will stay.</li>
           </ul>
         </div>
       );
@@ -255,6 +330,111 @@ const Css = ({ subSectionName }) => {
           </ul>
           <img alt="border box model" src="border-box-model.png" />
           <p>Overall it makes that the width and height of your element is not affected by the padding and border.</p>
+        </div>
+      );
+    }
+    case 'colors': {
+      return (
+        <div>
+          <p><b>RGB Model:</b></p>
+          <ul>
+            <li><b>RGB</b> = red, green, blue. Each parameter defines the intensity of that color and can be an integer between 0 and 255 or a percentage value (from 0% to 100%). Synthax: <Ic code="rgb(red, green, blue);" /></li>
+            <li><b>RGBA</b> = red, green, blue, alpha. Alows you to add a forth value between 0.0 and 1.0 define the transparency/opacity of your color. So <Ic code="color:transparent" /> = <Ic code="color: rgba(0, 0, 0, 0);" /></li>
+          </ul>
+          <p>In RGB system, if you put the same value for the 3 colors, you are on the grey scale. At 0 you are totally black and at 255 you are totally white. And in between you have greyscales:</p>
+          <img alt="rgb grayscale" src="../../assets/images/rgb-greyscale.png" />
+          <p><b>HSL Model:</b></p>
+          <p>HSL = hue, saturation, lightness. Synthax: <Ic code="color: hsl(120, 60%, 70%)" /> :</p>
+          <ul>
+            <li>first number = the degree of the hue (= an angle on a color wheel. Red = 0, Green = 120, Blue = 240)</li>
+            <li>second number = the saturation % = intensity or purity of the color (0% = grey, 100% = richest color)</li>
+            <li>third number = lightness % = how light or dark the color is (0% = black, 50% = normal, 100% = white)</li>
+          </ul>
+          <p>Useful if you just want to change the lightness or saturation of a color (more complex in RGB). Also usfull if you whant to have a set of colors that work well together: same saturation and lightness.</p>
+          <p>As for RGB you also have the <b>HSLA</b> model with transparency.</p>
+          <img alt="hsl wheel" src="../../assets/images/hsl-wheel.png" />
+        </div>
+      );
+    }
+    case 'typography': {
+      return (
+        <div>
+          <p>In CSS you have the system of fallback fonts:</p>
+          <PrismCode code={code.typography} language={language} />
+          <p>sThis says "use Garamond, if not available, use Times, if not available, any serif font pre-installed on the user device".</p>
+          <p><b>Font types are:</b></p>
+          <ul>
+            <li><Ic code="serif" /></li>
+            <li><Ic code="san-serif" /></li>
+            <li><Ic code="monospace" /></li>
+          </ul>
+          <p><b>Font weight</b> values range from 100 to 900 and can only be multiples of 100 (100, 200, 500...)</p>
+          <p><b>Font proprieties:</b></p>
+          <ul>
+            <li><Ic code="font-style: italic;" /></li>
+            <li><Ic code="word-spacing: Xem;" /> (by default = 0.25em)</li>
+            <li><Ic code="letter-spacing: Xem;" /></li>
+            <li><Ic code="text-transform:" />
+              <ul>
+                <li><Ic code="uppercase" /></li>
+                <li><Ic code="lowercase" /></li>
+                <li><Ic code="capitalize" /></li>
+              </ul>
+            </li>
+            <li><Ic code="line-height" /> (modify the leading of the text):
+              <ul>
+                <li>can take an unitless value, like "1.2". (It will define line height according to font size)</li>
+                <li>can take a unite value in px, %, em, rem...</li>
+              </ul>
+            </li>
+          </ul>
+          <img alt="css line height" src="../../assets/images/line-height.png" />
+          <p><b>Font-face:</b></p>
+          <p>You can use <Ic code="font-face" /> to load any font you want into the browser of your user. You can use it with your own font in your own server or with an external source. You need to specify :</p>
+          <ul>
+            <li>the name of the font</li>
+            <li>the direction of the font file</li>
+            <li>the format of the font file (Different browsers support different font types, so providing multiple font file options will support more browsers).</li>
+          </ul>
+          <PrismCode code={code.fontFace} language={language}/>
+        </div>
+      );
+    }
+    case 'grid': {
+      return (
+        <div>
+          <p>To work, a CSS grid needs a grid container and grid items.</p>
+          <p>Grid containers: must have the propriety: <Ic code="display: grid;" /></p>
+          <p>Inside containers you define <b>columns:</b></p>
+          <ul>
+            <li>By default grids contain 1 column</li>
+            <li>Then you can define the columns of a grid with the propriety: <Ic code="grid-template-columns: 200px 300px;" /> (This means there will be 2 columns, the first one of 300px and the second one of 300px)</li>
+            <li>You can define column width in px or % (of the total grid width), and you can mix between units in the same grid</li>
+          </ul>
+          <PrismCode code={code.gridColumns} language={language} />
+          <p>Inside containers you also define rows:</p>
+          <ul>
+            <li>Work the same way as columns, with the propriety : <Ic code="grid-template-rows: 200px 300px;" /> (only that it defines the height instead of the width. So if you define it in %, it is according to grid height)</li>
+            <li>By default, rows are sized to fit inside the grid</li>
+          </ul>
+          <p>You can also use one propriety for <b>both rows and columns:</b> <Ic code="colums: grid-template;" /></p>
+          <PrismCode code={code.gridTemplate} language={language} />
+          <p>(Before the / are rows, after are columns)</p>
+          <p><b>Fractions:</b> the <Ic code="fr" /> unit define the size of columns and rows as a fraction of grid length.</p>
+          <p><u>Ex:</u> <Ic code="grid-template: 2fr 1fr 1fr / 1fr 3fr 1fr;" /> >>> first row will be 2/4 of the grid, second row 1/4, third row 1/4. First column will be 1/5, second 3/5, third 1/5.</p>
+          <p>(If you use fr combined with other units ; the fr will represent a fraction of the available space.)</p>
+          <p><b>Repeat:</b> You can use the repeat fonction like that: <Ic code="grid-template-columns: repeat(3, 100px);" /> >>> is the same as writting : <Ic code="grid-template-columns: 100px 100px 100px;" /></p>
+          <p><b>Minmax:</b> <Ic code="grid-template-columns: 100px minmax(100px, 500px) 100px;" /> >>> the 2nd column will vary between 100px and 500px depending on the size of the overall grid. (For minmax to work your grid can't have a fixed size).</p>
+          <p><b>Grid Gap:</b></p>
+          <ul>
+            <li><Ic code="grid-row-gap: 10px" /> will add 10px between your rows</li>
+            <li><Ic code="grid-column-gap: 10px" /> will add 10px between your columns</li>
+            <li><Ic code="grid-gap: 20px 10px" /> will add 20px between rows and 10px between columns</li>
+          </ul>
+          <p><b>Grid items:</b></p>
+          <p><Ic code="grid-row-start" /> and <Ic code="grid-row-end" /> make a single grid items take up multiple rows. Same for <Ic code="grid-column-start" /> and <Ic code="grid-column-end" />.</p>
+          <PrismCode code={code.gridItem} language={language} />
+          <p>>>> The element .item will starts at row 1 and stops at row 3 (so it will take the space of 2 rows). Same for columns.</p>
         </div>
       );
     }
