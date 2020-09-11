8fc4e9488881cf2f379bdda0b70d60d1d2a41315
renaud dor <renaud.dor@gmail.com>
2020-09-11 08:34:52 -0500

    Finish Navbar sub sections animation

--- src/containers/navbar_sub_sections.jsx
+++ src/containers/navbar_sub_sections.jsx
@@ -2,33 +2,42 @@ import React, { Component } from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
 
 class NavbarSubSections extends Component {
-  componentWillReceiveProps(nextProps) {
-    const { section, subSections, selectedSection } = this.props;
-    if (nextProps.selectedSection === section && this.navSubLinks) {
+
+  componentDidMount() {
+    const { section, selectedSection } = this.props;
+    console.log(this.navSubLinks.clientHeight);
+    if (selectedSection === section && this.navSubLinks) {
+      this.navSubLinks.style = 'display: block;';
       this.navSubLinks.parentNode.style = `height: ${this.navSubLinks.clientHeight}px`;
     }
   }
 
+  componentDidUpdate() {
+    const { section, selectedSection } = this.props;
+    if (selectedSection === section) {
+      this.navSubLinks.style = 'display: block;';
+      this.navSubLinks.parentNode.style = `height: ${this.navSubLinks.clientHeight}px`;
+    } else {
+      this.navSubLinks.parentNode.style = 'height: 0';
+      setTimeout(() => { this.navSubLinks.parentNode.style = 'display: none;'; }, 500);
+    }
+  }
+
   render() {
-    const { section, subSections, selectedSection } = this.props;
-    if (section === selectedSection) {
-      return (
-        <div className="navbar-sub-sections">
-          <ul ref={(navSubLinks) => { this.navSubLinks = navSubLinks; }} className="nav-sub-links">
-            {subSections.map((subSection) => {
-              return (
-                <li key={subSection}>
-                  <Link className="nav-link sub-section-link" to={`/${section}#${subSection}`}>{subSection}</Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      );
-    }
-    else {
-      return null;
-    }
+    const { section, subSections } = this.props;
+    return (
+      <div className="navbar-sub-sections">
+        <ul ref={(navSubLinks) => { this.navSubLinks = navSubLinks; }} className="nav-sub-links">
+          {subSections.map((subSection) => {
+            return (
+              <li key={subSection}>
+                <Link className="nav-link sub-section-link" to={`/${section}#${subSection}`}>{subSection}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
   }
 }
 
