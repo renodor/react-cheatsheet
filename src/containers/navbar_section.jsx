7cd60551c79b559ddfeab3b97b9d2c137c9b5ae9
renaud dor <renaud.dor@gmail.com>
2020-09-12 15:36:17 -0500

    Change navbar chevron

--- src/containers/navbar_section.jsx
+++ src/containers/navbar_section.jsx
@@ -22,7 +22,7 @@ class NavbarSection extends Component {
 
     return (
       <li key={section}>
-        <button type="button" onClick={this.handleClick} aria-expanded={activeNavbarSection === section} className="nav-link section-link">
+        <button type="button" onClick={this.handleClick} is-open={(activeNavbarSection === section).toString()} className="nav-link section-link">
           <span>{section}</span>
           <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-chevron-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
